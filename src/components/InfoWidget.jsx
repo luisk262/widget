8cd927ff6212a85fb5b4959530d0ed4acc89334a
Luis Alvarez <luisk__@hotmail.com>
2022-10-26 19:33:59 -0500

    initial commit

---
+++ src/components/InfoWidget.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import calendar from "./calendar.svg"
+import contact from "./contact.svg"
+import phone from "./phone.svg"
+import wp from "./wp.svg"
+
+export const InfoWidget = () =>{
+
+    return (
+        <div id='floating_nav'>
+            <a href='#' id='reservation'>
+                <span>Reservar</span>
+                <img   src={ calendar}/>
+                </a>
+            <a href='#' id='contact'><span>Contáctanos</span> <img   src={ contact}/></a>
+            <a href='#' id='phone'><span>Teléfono</span><img   src={ phone}/></a>
+            <a href='#' id='whatsapp'><span>Whatsapp</span><img   src={ wp}/></a>
+        </div>
+    )
+}
