7728baa7e0daa90e9f85fb47857b50eb658e91b7
Luis Alvarez <luisk__@hotmail.com>
2022-11-10 22:54:52 -0500

    ajuste

--- src/components/FloatingMenu.js
+++ src/components/FloatingMenu.js
@@ -4,23 +4,38 @@ import LeadWidget from './LeadWidget'
 
 import calendar from "../assets/calendar.svg"
 import contact from "../assets/contact.svg"
-import phone from "../assets/phone.svg"
+// import phone from "../assets/phone.svg"
 import wp from "../assets/wp.svg"
 
-function FloatingMenu({data}) {
-    
+function FloatingMenu({ data }) {
+
     return (
         <div id='floating_nav'>
-            <a  id="calendar" > <span>Reservar</span> <img src={calendar} /></a>
-            <a id='contact' ><span>Contáctanos</span> <img style={{height: "20px"}} src={contact} /></a>
-            <a id='phone'><span>Teléfono</span><img src={phone} /></a>
-            <a id='whatsapp' ><span>Whatsapp</span><img src={wp} /></a>
+            {/* Menu */}
+                <ul className='menu'>
+                    <li className='menu_item'>
+                        <p>
+                            <a id="calendar"><span>Reservar</span> <img src={calendar} /></a>
+                        </p>
+                    </li>
+                    <li className='menu_item'>
+                        <p>
+                            <a id='contact' ><span>Contáctanos</span> <img style={{ height: "20px" }} src={contact} /></a>
+                        </p>
+                    </li>
+                    <li className='menu_item'>
+                        <p><a id='whatsapp' href='https://wa.me/573114579556' target="_blank" ><span>Whatsapp</span><img src={wp} /></a></p>
+                    </li>
+                </ul>
+              {/* End Menu */}
+            
+
+            {/* Body Widgets */}
             <div id="bewe-widget-container"></div>
             <div id="bewe-widget-container-lead"></div>
-            
             <CalendarWidget centerId={data.centerId} />
-            <LeadWidget centerId={data.centerId}/>
-            
+            <LeadWidget centerId={data.centerId} />
+             {/* End Body Widgets */}
         </div>
     );
 }
